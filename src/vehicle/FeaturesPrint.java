8ab59b19560e7c7de5a4e84a8f91ee8daebc5633
Aayush Bisani <98866843+abisani1711@users.noreply.github.com>
2022-02-10 11:20:17 +0530

    Update FeaturesPrint.java

--- src/vehicle/FeaturesPrint.java
+++ src/vehicle/FeaturesPrint.java
@@ -2,7 +2,7 @@ package vehicle;
 // import xml.DatabaseManager;
 
 public class FeaturesPrint {
-    public static void printSalary(Vehicle vehicle) {
+    public static void printVehicleInfo(Vehicle vehicle) {
         System.out.println("Name "+vehicle.getBrandName());
         System.out.println("Number of Airbags:" + vehicle.getNumberOfAirbags());
         System.out.println("Seating capacity:" + vehicle.getSeatingCapacity());
